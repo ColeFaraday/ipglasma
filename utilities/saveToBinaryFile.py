0c51fbb8e53691112ce8762582c1c7c2d93068c3
Coleridge Faraday <colefaraday@gmail.com>
2025-12-04 12:41:55 +0200

    Modify saveToBinaryFile.py to be more flexible. Add He4 from 1812.08096

--- utilities/saveToBinaryFile.py
+++ utilities/saveToBinaryFile.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python3
 """
-    This is an example code to save a python data array to a binary file
-    and c++ program can read from it. The data is saved with single float
-    precision (float32).
+    Save a python data array to a binary file that a C++ program can read.
+    The data is saved with single float precision (float32).
 """
 
+import argparse
 import array
 import numpy as np
 
-data = np.loadtxt("../carbon_alpha_3.in")
 
-outputFile = open("C12_alphaCluster.bin.in", "wb")
-for config_i in data:
-    float_array = array.array('f', config_i)
-    float_array.tofile(outputFile)
-outputFile.close()
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert a text data file to binary format (float32)."
+    )
+    parser.add_argument("input", help="Input text file to read")
+    parser.add_argument("output", help="Output binary file to write")
+    args = parser.parse_args()
+
+    data = np.loadtxt(args.input)
+
+    with open(args.output, "wb") as outputFile:
+        for config_i in data:
+            float_array = array.array('f', config_i)
+            float_array.tofile(outputFile)
+
+
+if __name__ == "__main__":
+    main()
