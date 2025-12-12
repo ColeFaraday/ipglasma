fbcadf6ce2387a6a326cfc44125a22d84352b342
Coleridge Faraday <colefaraday@gmail.com>
2025-12-12 16:39:28 +0200

    Add Li6 based on 3Fermi fit

--- src/Glauber.cpp
+++ src/Glauber.cpp
@@ -71,6 +71,17 @@ void Glauber::FindNucleusData2(Nucleus *nucleus, string name,
     nucleus->beta3 = 0.;
     nucleus->beta4 = 0.0;
     nucleus->gamma = 0.;
+  } else if (name.compare("Li6") == 0) {
+    nucleus->A = 6.;
+    nucleus->Z = 3.;
+    densityFunction = "3Fermi";
+    nucleus->R_WS = 1.355;
+    nucleus->w_WS = 0.;
+    nucleus->a_WS = 0.584;
+    nucleus->beta2 = 0.0;
+    nucleus->beta3 = 0.;
+    nucleus->beta4 = 0.0;
+    nucleus->gamma = 0.;
   } else if (name.compare("d") == 0) {
     nucleus->A = 2;
     nucleus->Z = 1;
